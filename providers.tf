8f3e226a3101c14864c91705feaf5241becedb4f
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-07 18:13:34 +0100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,14 @@
+provider "aviatrix" {
+  username     = "admin"
+  password      = var.ctrl_password
+  controller_ip = "18.156.141.82"
+  version       = "2.17.0"
+}
+
+
+provider "aws" {
+  version    = "~> 2.0"
+  region =  var.cloud_region["${var.aviatrix_transit_gateway}"]
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
