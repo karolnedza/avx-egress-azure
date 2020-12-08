60f4c859010ba3e20543ca45052518faabd18d0e
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-08 20:14:28 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,5 @@ variable "vnet_vpc_address_space" {}
 variable "cloud_region" {}
 variable "fqdn_domains" {}
 variable "vpc_name" {default = "default-name"}
-
-
 variable "ctrl_password" {}
 variable "vcs_repository" {default = "placeholder"}
