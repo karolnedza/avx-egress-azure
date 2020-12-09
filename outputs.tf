9fd054c3c2e4ae0eee7b3c41ff8c365bbb3f7a67
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-09 14:53:56 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,14 +3,14 @@ output "created_vpc_subnet" {
  }
 
 
-output "created_vpc_private_subnets" {
+output "created_private_subnets" {
  value = aviatrix_vpc.aviatrix_vpc_vnet.private_subnets[*].cidr
  }
 
-output "created_vpc_public_subnets" {
+output "created_public_subnets" {
  value = aviatrix_vpc.aviatrix_vpc_vnet.public_subnets[*].cidr
  }
 
 output "blocked_sites" {
-value = aviatrix_fqdn.fqdn_egress.domain_names 
+value = aviatrix_fqdn.fqdn_egress.domain_names.fqdn
 }
