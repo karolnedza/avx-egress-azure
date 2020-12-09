7df276037329f46ed02143944cccc33325c96663
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-09 14:21:28 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "created_vpc_subnet" {
  value = aviatrix_vpc.aviatrix_vpc_vnet.vpc_id
  }
 
-output "created_vpc_subnets" {
- value = aviatrix_vpc.aviatrix_vpc_vnet.subnets[*].cidr
+
+output "created_vpc_private_subnets" {
+ value = aviatrix_vpc.aviatrix_vpc_vnet.private_subnets[*].cidr
+ }
+
+output "created_vpc_public_subnets" {
+ value = aviatrix_vpc.aviatrix_vpc_vnet.public_subnets[*].cidr
  }
