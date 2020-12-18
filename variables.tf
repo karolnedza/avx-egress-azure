37be3a4ef7e72f1c1b6415526e28cb5ddee601cc
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-18 15:29:16 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "vnet_vpc_address_space" {}   
 variable "cloud_region" {}
-variable "fqdn_domains" {} # list of domains example:  facebook.com, *.tiktok.com, www,amazone
+variable "fqdn_domains" {} # list of domains example:  facebook.com, *.tiktok.com, www.amazon.com
 variable "vpc_name" {default = "default-name"}
 variable "ctrl_password" {}
 variable "vcs_repository" {default = "placeholder"}
